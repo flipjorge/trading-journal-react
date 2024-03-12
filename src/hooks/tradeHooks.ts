6fcc7605898591e5138e6bd80ec6f3951b5d6c80
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-12 17:50:59 +0000

    Create custom hooks for trades

---
+++ src/hooks/tradeHooks.ts
@@ -0,0 +1,42 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../store";
+import { Trade } from "../models/Models";
+import { useMemo } from "react";
+
+export const useGetAllTrades = () => {
+    return useSelector((state:RootState) => state.trades);
+}
+
+export const useGetTradesBySymbol = (symbol:string) => {
+    const trades = useGetAllTrades();
+    return trades.filter(trade => trade.symbol === symbol);
+}
+
+export const useTradeFirstDate = (trade:Trade) => {
+    return useMemo(() => {
+        if(!trade.transactions) return '-';
+        return trade.transactions[0].datetime.toLocaleDateString();
+    }, [trade.transactions]);
+}
+
+export const useTradeEntryPrice = (trade:Trade) => {
+    return useMemo(() => {
+        return trade.transactions[0]?.price || '-';
+    }, [trade.transactions]);
+}
+
+export const useTradeTotalQuantity = (trade:Trade) => {
+    return useMemo(() => {
+        return trade.transactions.reduce((total, transaction) => {
+            if(transaction.action === 'buy') return total + transaction.quantity;
+            return total - transaction.quantity;
+        }, 0);
+    }, [trade.transactions]);
+}
+
+export const useTradeEntryTotal = (trade:Trade) => {
+    return useMemo(() => {
+        if(!trade.transactions) return '-';
+        return trade.transactions[0].price * trade.transactions[0].quantity;
+    }, [trade.transactions]);
+}
