445713a07e75668289877b0c551a43786693f4fa
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-12 17:50:01 +0000

    Fix store reducer

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import tradesReducer from './slices/tradesSlice';
 
+const rootReducer = combineReducers({
+    trades:tradesReducer
+})
+
 export const store = configureStore({
-    reducer: {
-        trades: tradesReducer
-    }
+    reducer: rootReducer
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
