b6a0d811af93f3b8ca2ac3dc3b51d83cf74e7408
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-12 15:37:20 +0000

    Move style in TradesPage

--- src/pages/TradesPage.tsx
+++ src/pages/TradesPage.tsx
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 import AddTradeButton from "../components/AddTradeButton";
 
+const AddTradeButtonContainer = styled.div`
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+`
+
 const TradesPage = () => {
-
-    const AddTradeButtonContainer = styled.div`
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-    `
-
     return <div>
         <AddTradeButtonContainer>
             <AddTradeButton/>
