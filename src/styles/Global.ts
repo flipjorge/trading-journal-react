ff177a3f89a995b315a4cc8dd455e7466d913530
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-12 17:50:26 +0000

    Import roboto font

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
     * {
         margin: 0;
         padding: 0;
+        font-family: 'Roboto', sans-serif;
     }
 `
 
