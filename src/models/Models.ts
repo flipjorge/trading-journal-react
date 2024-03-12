56842e33306463d645c8c4f380dfae026be60a68
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-12 15:14:55 +0000

    Add id properties to trade models

--- src/models/Models.ts
+++ src/models/Models.ts
@@ -1,9 +1,11 @@
 export interface Trade {
+    id: number,
     symbol: string,
     transactions: TradeTransaction[]
 }
 
 export interface TradeTransaction {
+    id: number,
     action: 'buy' | 'sell',
     datetime: Date,
     quantity: number,
