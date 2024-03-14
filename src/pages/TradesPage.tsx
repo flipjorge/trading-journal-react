7e7387dfaba1f70f0e3ec2462074a48240412dbc
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-14 17:18:13 +0000

    Add AddTradeDialog to trades page

--- src/pages/TradesPage.tsx
+++ src/pages/TradesPage.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import AddTradeButton from "../components/AddTradeButton";
 import TradesList from "../components/TradesList";
 import { useGetAllTrades } from "../hooks/tradeHooks";
+import AddTradeDialog from "../components/AddTradeDialog";
+import { useRef, useState } from "react";
 
 const AddTradeButtonContainer = styled.div`
     position: fixed;
@@ -9,15 +11,46 @@ const AddTradeButtonContainer = styled.div`
     right: 20px;
 `
 
+const AddTradeDialogContainer = styled.div<{isVisible:boolean}>`
+    position: fixed;
+    display: ${ ({isVisible}) => isVisible ? 'flex' : 'none' };
+    width: 100%;
+    height: 100%;
+    top:0;
+    z-index: 100;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0,0,0,.2);
+    backdrop-filter: blur(2px);
+`
+
 const TradesPage = () => {
 
     const trades = useGetAllTrades();
 
+    const [isAddTradeDialogVisible, setIsAddTradeDialogVisible] = useState(false);
+    const addTradeDialogRef = useRef(null);
+
+    const handleAddTradeOpen = () => {
+        setIsAddTradeDialogVisible(true);
+    }
+
+    const handleAddTradeClose = (e:React.MouseEvent) => {
+        if(addTradeDialogRef.current !== e.target) return;
+        setIsAddTradeDialogVisible(false);
+    }
+
     return <div>
         <TradesList trades={trades}/>
         <AddTradeButtonContainer>
-            <AddTradeButton/>
+            <AddTradeButton onClick={handleAddTradeOpen}/>
         </AddTradeButtonContainer>
+        <AddTradeDialogContainer
+            ref={addTradeDialogRef}
+            isVisible={isAddTradeDialogVisible}
+            onClick={handleAddTradeClose}>
+            <AddTradeDialog/>
+        </AddTradeDialogContainer>
     </div>;
 }
 
