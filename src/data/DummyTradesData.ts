3bec583a6abc1d7bef3cfe2149b36541bf8fc448
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-17 17:25:25 +0000

    Edit dummy data

--- src/data/DummyTradesData.ts
+++ src/data/DummyTradesData.ts
@@ -2,59 +2,14 @@ import { Trade } from "../models/tradeModels"
 
 export const dummyData:Trade[] = [
     {
-        id: 0,
-        symbol: 'btc',
-        transactions: [{
-            id:0,
-            action:'buy',
-            datetime: new Date("2024-03-10 14:30:00").toISOString(),
-            price: 12000,
-            quantity: 1,
-            fee: 0.0001
-        },{
-            id:1,
-            action:'sell',
-            datetime: new Date("2024-03-10 14:40:00").toISOString(),
-            price: 14000,
-            quantity: 1,
-            fee: 0.0001
-        }]
+        id: '0',
+        symbol: 'btc'
     },
     {
-        id: 1,
-        symbol: 'btc',
-        transactions: [{
-            id:0,
-            action:'buy',
-            datetime: new Date("2024-03-11 14:30:00").toISOString(),
-            price: 12000,
-            quantity: 1.6,
-            fee: 0.0001
-        },{
-            id:1,
-            action:'sell',
-            datetime: new Date("2024-03-11 14:40:00").toISOString(),
-            price: 14000,
-            quantity: 0.6,
-            fee: 0.0001
-        },{
-            id:2,
-            action:'sell',
-            datetime: new Date("2024-03-11 14:50:00").toISOString(),
-            price: 14450,
-            quantity: 1,
-            fee: 0.00011
-        }]
+        id: '1',
+        symbol: 'btc'
     },{
-        id: 2,
-        symbol: 'eth',
-        transactions: [{
-            id:0,
-            action:'buy',
-            datetime: new Date("2024-03-12 10:15:00").toISOString(),
-            price: 1700,
-            quantity: 3,
-            fee: 0.02
-        }]
+        id: '2',
+        symbol: 'eth'
     }
 ];
---
+++ src/data/dummyTransactionsData.ts
@@ -0,0 +1,53 @@
+import { TradeTransaction } from "../models/tradeModels";
+
+export const dummyTransactionsData:TradeTransaction[] = [
+    {
+        id:'0',
+        tradeId:'0',
+        action:'buy',
+        datetime: new Date("2024-03-10 14:30:00").toISOString(),
+        price: 12000,
+        quantity: 1,
+        fee: 0.0001
+    },{
+        id:'1',
+        tradeId:'0',
+        action:'sell',
+        datetime: new Date("2024-03-10 14:40:00").toISOString(),
+        price: 14000,
+        quantity: 1,
+        fee: 0.0001
+    },{
+        id:'2',
+        tradeId:'1',
+        action:'buy',
+        datetime: new Date("2024-03-11 14:30:00").toISOString(),
+        price: 12000,
+        quantity: 1.6,
+        fee: 0.0001
+    },{
+        id:'3',
+        tradeId:'1',
+        action:'sell',
+        datetime: new Date("2024-03-11 14:40:00").toISOString(),
+        price: 14000,
+        quantity: 0.6,
+        fee: 0.0001
+    },{
+        id:'4',
+        tradeId:'1',
+        action:'sell',
+        datetime: new Date("2024-03-11 14:50:00").toISOString(),
+        price: 14450,
+        quantity: 1,
+        fee: 0.00011
+    },{
+        id:'5',
+        tradeId:'2',
+        action:'buy',
+        datetime: new Date("2024-03-12 10:15:00").toISOString(),
+        price: 1700,
+        quantity: 3,
+        fee: 0.02
+    }
+]
