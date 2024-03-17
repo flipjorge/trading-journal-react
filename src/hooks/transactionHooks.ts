6c42767b78de64d80e36d602be333c918c9debff
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-17 18:31:39 +0000

    Create new actions and hooks for add, update, remove and set multiple transactions

--- src/hooks/transactionHooks.ts
+++ src/hooks/transactionHooks.ts
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../store";
 import { useMemo } from "react";
 import { TradeTransaction } from "../models/tradeModels";
-import { addTransaction, updateTransaction, removeTransaction } from "../slices/transactionsSlice";
+import { addTransaction, updateTransaction, removeTransaction, addTransactions, updateTransactions, removeTransactionsByTradeId, setTransactionsForTrade } from "../slices/transactionsSlice";
 
 export const useGetAllTransactions = () => {
     return useSelector((state:RootState) => state.transactions);
@@ -21,14 +21,39 @@ export const useAddTransaction = () => {
     return (transaction:TradeTransaction) => dispatch(addTransaction(transaction));
 }
 
+export const useAddTransactions = () => {
+    const dispatch = useDispatch();
+
+    return (transactions:TradeTransaction[]) => dispatch(addTransactions(transactions));
+}
+
 export const useUpdateTransaction = () => {
     const dispatch = useDispatch();
 
     return (transaction:TradeTransaction) => dispatch(updateTransaction(transaction));
 }
 
+export const useUpdateTransactions = () => {
+    const dispatch = useDispatch();
+
+    return (transactions:TradeTransaction[]) => dispatch(updateTransactions(transactions));
+}
+
+export const useSetTransactionsForTrade = () => {
+    const dispatch = useDispatch();
+
+    return (tradeId:string, transactions:TradeTransaction[]) => 
+        dispatch(setTransactionsForTrade({tradeId, transactions}));
+}
+
 export const useRemoveTransaction = () => {
     const dispatch = useDispatch();
 
     return (transactionId:string) => dispatch(removeTransaction(transactionId));
+}
+
+export const useRemoveTransactionsByTradeId = () => {
+    const dispatch = useDispatch();
+
+    return (tradeId:string) => dispatch(removeTransactionsByTradeId(tradeId));
 }
--- src/slices/transactionsSlice.ts
+++ src/slices/transactionsSlice.ts
@@ -9,20 +9,66 @@ const transactionsSlice = createSlice({
         addTransaction: (state:TradeTransaction[], action:PayloadAction<TradeTransaction>) => {
             state.push(action.payload);
         },
+        addTransactions: (state:TradeTransaction[], action:PayloadAction<TradeTransaction[]>) => {
+            state.push(...action.payload);
+        },
         updateTransaction: (state:TradeTransaction[], action:PayloadAction<TradeTransaction>) => {
             const index = state.findIndex(transaction => transaction.id === action.payload.id);
             if(index !== -1) {
                 state[index] = action.payload;
             }
         },
+        updateTransactions: (state:TradeTransaction[], action:PayloadAction<TradeTransaction[]>) => {
+            action.payload.forEach(actionTransaction => {
+                const index = state.findIndex(stateTransaction => stateTransaction.id === actionTransaction.id);
+                if(index !== -1) {
+                    state[index] = actionTransaction;
+                }
+            });
+        },
+        setTransactionsForTrade: (state:TradeTransaction[], action:PayloadAction<{tradeId:string, transactions:TradeTransaction[]}>) => {
+            
+            const tradeTransactions = state.filter(stateTransaction => stateTransaction.tradeId === action.payload.tradeId);
+
+            //delete from state transactions not presented in action payload
+            tradeTransactions.forEach(tradeTransaction => {
+                const found = action.payload.transactions.find(actionTransaction => actionTransaction.id === tradeTransaction.id);
+                if(!found) {
+                    const index = state.findIndex(stateTransaction => stateTransaction.id === tradeTransaction.id);    
+                    if(index !== -1) {
+                        state.splice(index);
+                    }
+                }
+            });
+
+            action.payload.transactions.forEach(actionTransaction => {
+                const index = state.findIndex(stateTransaction => stateTransaction.id === actionTransaction.id);
+                if(index !== -1) {
+                    //update transaction
+                    state[index] = actionTransaction;
+                } else {
+                    //add transaction if its new
+                    state.push(actionTransaction);
+                }
+            });
+        },
         removeTransaction: (state:TradeTransaction[], action:PayloadAction<string>) => {
             const index = state.findIndex(transaction => transaction.id === action.payload);
             if(index !== -1) {
                 state.splice(index, 1);
             }
+        },
+        removeTransactionsByTradeId: (state:TradeTransaction[], action:PayloadAction<string>) => {
+            console.log(state.length);
+            state.forEach(() => {
+                const index = state.findIndex(transaction => transaction.tradeId === action.payload);
+                if(index !== -1) {
+                    state.splice(index, 1);
+                }
+            })
         }
     }
 });
 
-export const { addTransaction, updateTransaction, removeTransaction } = transactionsSlice.actions;
+export const { addTransaction, addTransactions, updateTransaction, updateTransactions, setTransactionsForTrade, removeTransaction, removeTransactionsByTradeId } = transactionsSlice.actions;
 export default transactionsSlice.reducer;
