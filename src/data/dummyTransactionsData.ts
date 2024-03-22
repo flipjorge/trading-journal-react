e036a58016abdc146b58e2454e96206535721cbb
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2024-03-22 13:29:06 +0000

    Fix datatime on dummy transactions data

--- src/data/dummyTransactionsData.ts
+++ src/data/dummyTransactionsData.ts
@@ -1,11 +1,12 @@
 import { Transaction } from "../models/tradeModels";
+import { convertDateToInputFormat } from "../utils/dateTimeUtils";
 
 export const dummyTransactionsData:Transaction[] = [
     {
         id:'0',
         tradeId:'0',
         action:'buy',
-        datetime: new Date("2024-03-10 14:30:00").toISOString(),
+        datetime: convertDateToInputFormat(new Date("2024-03-10 14:30:00")),
         price: 12000,
         quantity: 1,
         fee: 0.0001
@@ -13,7 +14,7 @@ export const dummyTransactionsData:Transaction[] = [
         id:'1',
         tradeId:'0',
         action:'sell',
-        datetime: new Date("2024-03-10 14:40:00").toISOString(),
+        datetime: convertDateToInputFormat(new Date("2024-03-10 14:40:00")),
         price: 14000,
         quantity: 1,
         fee: 0.0001
@@ -21,7 +22,7 @@ export const dummyTransactionsData:Transaction[] = [
         id:'2',
         tradeId:'1',
         action:'buy',
-        datetime: new Date("2024-03-11 14:30:00").toISOString(),
+        datetime: convertDateToInputFormat(new Date("2024-03-11 14:30:00")),
         price: 12000,
         quantity: 1.6,
         fee: 0.0001
@@ -29,7 +30,7 @@ export const dummyTransactionsData:Transaction[] = [
         id:'3',
         tradeId:'1',
         action:'sell',
-        datetime: new Date("2024-03-11 14:40:00").toISOString(),
+        datetime: convertDateToInputFormat(new Date("2024-03-11 14:40:00")),
         price: 14000,
         quantity: 0.6,
         fee: 0.0001
@@ -37,7 +38,7 @@ export const dummyTransactionsData:Transaction[] = [
         id:'4',
         tradeId:'1',
         action:'sell',
-        datetime: new Date("2024-03-11 14:50:00").toISOString(),
+        datetime: convertDateToInputFormat(new Date("2024-03-11 14:50:00")),
         price: 14450,
         quantity: 1,
         fee: 0.00011
@@ -45,7 +46,7 @@ export const dummyTransactionsData:Transaction[] = [
         id:'5',
         tradeId:'2',
         action:'buy',
-        datetime: new Date("2024-03-12 10:15:00").toISOString(),
+        datetime: convertDateToInputFormat(new Date("2024-03-12 10:15:00")),
         price: 1700,
         quantity: 3,
         fee: 0.02
